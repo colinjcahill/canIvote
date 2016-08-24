0c403cf1f464b0e669d991e033dad7a728138d59
Colin Cahill <colincahill@gmail.com>
2016-08-24 01:25:44 -0700

    began first view

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,3 +1,5 @@
 class PagesController < ApplicationController
-
+  def index
+    @states = YAML.load_file('./app/models/states.yml')
+  end
 end
---
+++ app/models/polls.rb
@@ -0,0 +1,5 @@
+class Poll
+  require 'yaml'
+  require 'pollster'
+
+end
