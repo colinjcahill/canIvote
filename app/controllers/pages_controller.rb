595e70d43579b62c339a4f9c11ad767bb88e72f3
Colin Cahill <colincahill@gmail.com>
2016-09-05 15:54:22 -0700

    drawing ever closer to complete logic on poll

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,6 +2,11 @@ class PagesController < ApplicationController
   include Charts
   def index
     @states = YAML.load_file('./lib/states.yml')
+    @states.keys.each do |state|
+      poll = Poll.new
+      poll.search_and_parse state
+      @states << poll
+    end
     binding.pry
   end
 end
--- app/models/charts.rb
+++ app/models/charts.rb
@@ -4,7 +4,7 @@ module Charts
     require 'yaml'
     require 'pollster'
 
-    attr_accessor :date, :vote_for_jill_bool, :clinton_poll, :trump_poll, :other_poll, :undecided_poll
+    attr_accessor :date, :vote_for_jill_bool, :clinton_poll, :trump_poll, :other_poll, :undecided_poll, :no_data
 
     def initialize
       @date = nil
@@ -13,18 +13,27 @@ module Charts
       @trump_poll = nil
       @other_poll = nil
       @undecided_poll = nil
+      @no_data = nil
     end
 
     def poll_by_state(state)
       states = YAML.load_file('./lib/states.yml')
       chart = Chart.where(:topic => "2016-president", :state =>"#{states[state]}").first
-      chart.estimates_by_date.first
+      chart ? chart.estimates_by_date.first : nil
     end
 
     def search_and_parse(state_fullname)
       result = poll_by_state(state_fullname)
-      @date = result[:date]
-      parsed = result[:estimates].map {|i| i.invert.keys}.flatten
+      if result
+        @date = result[:date]
+        parsed = result[:estimates].map {|i| Hash[*i.invert.keys]}.flatten.inject(:merge)
+        @trump_poll = parsed["Trump"]
+        @clinton_poll = parsed["Clinton"]
+        @other_poll = parsed["Other"]
+        @undecided_poll = parsed["Undecided"]
+      else
+        @no_data = true
+      end
     end
   end
 end
